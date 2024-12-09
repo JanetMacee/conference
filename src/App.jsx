c8e6acb8f35a6aff9c0bb276fd33b83399a6168b
unknown <janetannanmark@gmail.com>
2024-12-09 10:10:57 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Routers from "./conference/pages/navigation/routes";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Routers />
+      </div>
+    </>
+  );
+}
+
+export default App;
